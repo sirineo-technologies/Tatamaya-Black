4614d1eeee88bc063acc68d660ee921755d41e4a
engereggae <sirineotechnologies.adm@gmail.com>
2022-03-07 23:40:51 -0300

    Codigos da Tatamaya Black 2

---
+++ Codigos/ledrgb/ledrgb.ino
@@ -0,0 +1,53 @@
+const int redpin=5;      //Pin 9
+const int greenpin=11;    //Pin 10
+const int bluepin=2;      //Pin 11
+
+
+int var=0;
+int var1=0;
+
+
+void setup()
+{
+}
+
+void loop()
+{
+  for(var=250;var<255;var++)
+  {
+  analogWrite(redpin,var);  //RED
+  analogWrite(greenpin,0);
+  delay(500);
+  analogWrite(redpin,0);    //GREEN
+  analogWrite(greenpin,var);
+  delay(500);
+  analogWrite(greenpin,0);   //BLUE
+  analogWrite(bluepin,var);
+  delay(500);
+  analogWrite(bluepin,0);
+  delay(500);
+  }
+  
+  for(var1=250;var1<255;var1++)
+  {
+  analogWrite(redpin,var1);   //YELLOW
+  analogWrite(greenpin,var1);
+  delay(500);
+  analogWrite(redpin,0);
+  delay(500);
+  analogWrite(greenpin,var1);  //CYAN
+  analogWrite(bluepin,var1);
+  delay(500);
+  analogWrite(greenpin,0);
+  delay(500);
+  analogWrite(bluepin,var1);    //MAGENTA
+  analogWrite(redpin,var1);
+  delay(500);
+  analogWrite(bluepin,0);
+  delay(500);
+  analogWrite(bluepin,var1);
+  analogWrite(redpin,var1);
+  analogWrite(greenpin,var1);
+  }
+  
+}
